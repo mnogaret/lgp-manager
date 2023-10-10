a58a8892acbc8fc455869945ffb07a091da160b7
Mathieu <mathieu.nogaret@gmail.com>
2023-10-10 08:54:45 +0200

    deployment

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Variables
+LOCAL_PATH="."
+REMOTE_USER="lyonglac"
+REMOTE_HOST="51.68.11.192"
+REMOTE_PATH="/homez.906/lyonglac/www3"
+
+# Synchronisation avec rsync
+#rsync -avzp -vvv --chmod=Dug=rwx,Do=,Fug=rw,Fo= --delete --exclude='.env' $LOCAL_PATH $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+rsync -avzp -vvv --no-perms --delete --exclude='.env' $LOCAL_PATH $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+
+echo "Déploiement terminé."
