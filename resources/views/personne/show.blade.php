e71dec962c21f543e98cffccdce398bba2089be2
unknown <mathieu.nogaret@gmail.com>
2023-10-31 19:24:56 +0100

    blade component

--- resources/views/personne/show.blade.php
+++ resources/views/personne/show.blade.php
@@ -3,19 +3,20 @@
 @section('content')
 <div class="container">
     <h1>Détails de la personne</h1>
-    <p><strong>Nom :</strong> {{ $personne->nom }}</p>
-    <p><strong>Prénom :</strong> {{ $personne->prenom }}</p>
-    <p><strong>Sexe :</strong> {{ $personne->sexe }}</p>
-    <p><strong>Nationalité :</strong> {{ $personne->nationalite }}</p>
-    <p><strong>Date de naissance :</strong> {{ $personne->date_naissance }}</p>
-    <p><strong>Ville de naissance :</strong> {{ $personne->ville_naissance }}</p>
-    <p><strong>Email 1 :</strong> {{ $personne->email1 }}</p>
-    <p><strong>Email 2 :</strong> {{ $personne->email2 }}</p>
-    <p><strong>Téléphone 1 :</strong> {{ $personne->telephone1 }}</p>
-    <p><strong>Téléphone 2 :</strong> {{ $personne->telephone2 }}</p>
-    <p><strong>Adresse postale :</strong> {{ $personne->adresse_postale }}</p>
-    <p><strong>Code postal :</strong> {{ $personne->code_postal }}</p>
-    <p><strong>Ville :</strong> {{ $personne->ville }}</p>
-    <p><strong>Numéro de licence :</strong> {{ $personne->numero_licence }}</p>
+
+    <x-input label="Nom" value="{{ $personne->nom }}" />
+    <x-input label="Prénom" value="{{ $personne->prenom }}" />
+    <x-input label="Sexe" value="{{ $personne->sexe }}" />
+    <x-input label="Nationalité" value="{{ $personne->nationalite }}" />
+    <x-input label="Date de naissance" value="{{ $personne->date_naissance }}" />
+    <x-input label="Ville de naissance" value="{{ $personne->ville_naissance }}" />
+    <x-input label="Email 1" value="{{ $personne->email1 }}" />
+    <x-input label="Email 2" value="{{ $personne->email2 }}" />
+    <x-input label="Téléphone 1" value="{{ $personne->telephone1 }}" />
+    <x-input label="Téléphone 2" value="{{ $personne->telephone2 }}" />
+    <x-input label="Adresse postale" value="{{ $personne->adresse_postale }}" />
+    <x-input label="Code postal" value="{{ $personne->code_postal }}" />
+    <x-input label="Ville" value="{{ $personne->ville }}" />
+    <x-input label="Numéro de licence" value="{{ $personne->numero_licence }}" />
 </div>
 @endsection
