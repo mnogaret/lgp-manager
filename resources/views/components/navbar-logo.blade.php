6d5a06262425fb9b416d64befeee739cc2f8ab45
Mathieu <mathieu.nogaret@gmail.com>
2023-11-01 08:33:22 +0100

    fix logo

--- resources/views/components/navbar-logo.blade.php
+++ resources/views/components/navbar-logo.blade.php
@@ -1,4 +1,4 @@
 <span class="flex items-center">
-    <img src="../assets/img/lgp-logo-light.png" class="h-16" alt="Flowbite Logo" />
+    <img src="{{ asset('assets/img/lgp-logo-light.png') }}" class="h-16" alt="LGP" />
     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">LGP Manager</span>
 </span>
