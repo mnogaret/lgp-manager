44efa26f22eaec9cdf881b92033ecdc19ed0c2d3
Mathieu <mathieu.nogaret@gmail.com>
2024-09-29 18:46:50 +0200

    Données de la fiche

--- app/Http/Controllers/FicheController.php
+++ app/Http/Controllers/FicheController.php
@@ -8,8 +8,20 @@ class FicheController extends Controller
 {
     public function show($hash_code)
     {
+        $saisonId = session('saison_id');
+        if (!$saisonId) {
+            return redirect()->route('welcome')->withErrors('Aucune saison sélectionnée');
+        }
+
         // Récupérer l'adhérent en fonction du hash_code
-        $adherent = Personne::where('hash_code', $hash_code)->with('adhesions.groupe')->first();
+        $adherent = Personne::where('hash_code', $hash_code)
+            ->with(['adhesions' => function ($query) use ($saisonId) {
+                // Filtrer les adhésions pour n'inclure que celles de la saison sélectionnée
+                $query->whereHas('groupe', function ($query) use ($saisonId) {
+                    $query->where('saison_id', $saisonId);
+                });
+            }, 'passages'])
+            ->first();
 
         // Si aucun adhérent n'est trouvé, retourner une erreur 404
         if (!$adherent) {
--- resources/views/fiche/show.blade.php
+++ resources/views/fiche/show.blade.php
@@ -1,19 +1,23 @@
 @extends('layouts.pointage')
 
 @section('content')
-    <h1>Fiche de l'adhérent : {{ $adherent->prenom }} {{ $adherent->nom }}</h1>
+    <h1>{{ $adherent->prenom }} {{ $adherent->nom }}</h1>
 
-    <p><strong>Email:</strong> {{ $adherent->email1 }}</p>
     <p><strong>Date de naissance:</strong> {{ $adherent->date_naissance }}</p>
-    <p><strong>Sexe:</strong> {{ $adherent->sexe }}</p>
     <p><strong>Numéro de licence:</strong> {{ $adherent->numero_licence }}</p>
+    <p><strong>Niveau:</strong> {{ $adherent->niveau }}</p>
 
     @if($adherent->adhesions->isNotEmpty())
-        <h2>Groupes d'adhésion</h2>
+        @foreach($adherent->adhesions as $adhesion)
+            <p><strong>Groupe:</strong> {{ $adhesion->groupe->nom }} ({{ $adhesion->etat }})</p>
+        @endforeach
+        <p><strong>Passages de lame:</strong></p>
         <ul>
-            @foreach($adherent->adhesions as $adhesion)
-                <li>{{ $adhesion->groupe->nom }} ({{ $adhesion->etat }})</li>
-            @endforeach
+            @forelse ($adherent->passages as $passage)
+                <li>{{ $passage->date }} - {{ $passage->niveau }} - {{ $passage->etat }}</li>
+            @empty
+                <li>Aucun passage de lame enregistré.</li>
+            @endforelse
         </ul>
     @endif
 @endsection
